c1eff52c38d5629be149c7fc86d2ee8bebcbb038
Phillip Zhang <p.f.zeratul@gmail.com>
2019-08-27 16:04:27 +0800

    Trying Simple Loading Cube

---
+++ Assets/Scripts/LoadCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+
+public class LoadCube : MonoBehaviour
+{
+    [SerializeField] private string assetAddress;
+    [SerializeField] private GameObject myGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadAsset(assetAddress);
+    }
+
+    void LoadAsset(string addressName)
+    {   
+        Addressables.LoadAssetAsync<GameObject>(addressName).Completed += LoadDone;
+    }
+
+    void LoadDone(AsyncOperationHandle<GameObject> obj)
+    {
+        myGameObject = obj.Result;
+        Debug.Log("Finished loading asset");
+        Instantiate(myGameObject, Vector3.zero, Quaternion.identity);
+    }
+
+}
