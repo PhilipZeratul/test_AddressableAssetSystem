2b02ea78e000bd9af01e2a95c3ddca5596e2693d
Phillip Zhang <p.f.zeratul@gmail.com>
2019-08-28 18:01:17 +0800

    Added a Variant Test

--- Assets/Scripts/LoadCube.cs
+++ Assets/Scripts/LoadCube.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Collections;
 
 
 public class LoadCube : MonoBehaviour
@@ -8,22 +9,45 @@ public class LoadCube : MonoBehaviour
     [SerializeField] private string assetAddress;
     [SerializeField] private GameObject myGameObject;
 
-    // Start is called before the first frame update
-    void Start()
+    private bool isDone = false;
+    private WaitForSeconds waitForHalfSecond = new WaitForSeconds(0.5f);
+
+    private IEnumerator Start()
     {
-        LoadAsset(assetAddress);
+        //LoadAsset(assetAddress);
+
+        while (true)
+        {
+            //if (isDone)
+            //{
+                //Instantiate(myGameObject, Vector3.zero, Quaternion.identity);
+                InstantiateAsset(assetAddress);                
+            //}
+            yield return waitForHalfSecond;
+        }
     }
 
-    void LoadAsset(string addressName)
+    private void LoadAsset(string address)
     {   
-        Addressables.LoadAssetAsync<GameObject>(addressName).Completed += LoadDone;
+        Addressables.LoadAssetAsync<GameObject>(address).Completed += LoadDone;
     }
 
-    void LoadDone(AsyncOperationHandle<GameObject> obj)
+    private void LoadDone(AsyncOperationHandle<GameObject> obj)
     {
-        myGameObject = obj.Result;
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+            myGameObject = obj.Result;
+
+        Addressables.Release(obj);
         Debug.Log("Finished loading asset");
-        Instantiate(myGameObject, Vector3.zero, Quaternion.identity);
     }
 
+    private void InstantiateAsset(string address)
+    {
+        Addressables.InstantiateAsync(address).Completed += InstantiateDone;
+    }
+
+    private void InstantiateDone(AsyncOperationHandle<GameObject> obj)
+    {
+        Debug.Log("Finished instantiating asset");
+    }
 }
---
+++ Assets/Scripts/VariantTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+
+public class VariantTest : MonoBehaviour
+{
+    public Material mat;
+
+
+    private void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    public void LoadHD()
+    {
+        LoadTexture("tree", "HD");
+    }
+
+    public void LoadSD()
+    {
+        LoadTexture("tree", "SD");
+    }
+
+    private void LoadTexture(string address, string label)
+    {
+        //Addressables.LoadAssetAsync<Texture2D>("tree").Completed
+        //Addressables.LoadAssetAsync<Texture2D>(new List<object> { address, label }).Completed
+        //    += TextureLoaded;
+
+        Addressables.LoadAssetsAsync<Texture2D>(new List<object> { address, label }, null, Addressables.MergeMode.Intersection).Completed
+            += TextureLoaded;
+    }
+
+    private void TextureLoaded(AsyncOperationHandle<IList<Texture2D>> obj)
+    {
+        mat.mainTexture = obj.Result[0];
+    }
+}
